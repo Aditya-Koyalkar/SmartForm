1f8ec6d8e36a9822cf4ab90ce0096fb1a621c361
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-09-23 20:14:00 +0530

    added hero section

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="p-3 shadow-md border">
+    <div className="p-5 shadow-md border">
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-1">
-          <Image src={"/logo.svg"} alt="logo" width={20} height={20} />
+          <Image src={"/logo.svg"} alt="logo" width={30} height={30} />
           <div className="text-primary font-semibold font-mono">
             Smart Form AI
           </div>
---
+++ app/_components/Hero.tsx
@@ -0,0 +1,40 @@
+const Hero = () => {
+  return (
+    <section className="bg-gray-50">
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen ">
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            Build Form in
+            <strong className="font-extrabold text-primary sm:block">
+              {" "}
+              Seconds not Hours{" "}
+            </strong>
+          </h1>
+
+          <p className="mt-4 sm:text-xl/relaxed text-slate-600">
+            Create custom forms in seconds with our intuitive AI form builder.
+            Designed for ease and efficiency!
+          </p>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+              className="block w-full font-bold rounded bg-primary px-12 py-3 text-sm  text-white shadow hover:bg-purple-800 focus:outline-none focus:ring active:bg-primary sm:w-auto"
+              href="#"
+            >
+              + Create Form with AI
+            </a>
+
+            <a
+              className="block w-full border-2 rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-purple-800 focus:outline-none focus:ring active:text-primary sm:w-auto"
+              href="#"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
