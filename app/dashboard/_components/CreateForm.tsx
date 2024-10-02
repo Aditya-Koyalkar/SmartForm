0202e1ea7c9bc09a238336ccd09d0e85177973a3
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-10-02 14:51:07 +0530

    fix the dialog

--- app/dashboard/_components/CreateForm.tsx
+++ app/dashboard/_components/CreateForm.tsx
@@ -9,21 +9,21 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Textarea } from "@/components/ui/textarea";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const CreateForm = () => {
-  //const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
   const [userInput, setUserInput] = useState<string>("");
   const onCreateForm = () => {
     console.log(userInput);
   };
   return (
     <div>
-      <Dialog>
+      <Dialog open={open}>
         <DialogTrigger>
-          <Button>+ Create new Form</Button>
+          <Button onClick={() => setOpen(true)}>+ Create new Form</Button>
         </DialogTrigger>
-        <DialogContent>
+        <DialogContent className="[&>button]:hidden">
           <DialogHeader>
             <DialogTitle>Create new Form</DialogTitle>
             <DialogDescription>
@@ -33,7 +33,9 @@ export const CreateForm = () => {
                 placeholder="Write the description about the Form you want to create"
               />
               <div className="flex justify-end gap-3 mt-3">
-                <Button variant={"destructive"}>Cancel</Button>
+                <Button onClick={() => setOpen(false)} variant={"destructive"}>
+                  Cancel
+                </Button>
                 <Button onClick={onCreateForm}>Create</Button>
               </div>
             </DialogDescription>
