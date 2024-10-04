1fe09f4bf5a0b094f45df6f4df08d8e8e9d7f862
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-10-04 11:27:53 +0530

    added how it works

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,8 +5,8 @@ import { IoMdShare } from "react-icons/io";
 
 const Hero = () => {
   return (
-    <section className=" bg-[#f2f2f2]">
-      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen flex-col">
+    <section className=" bg-[#f2f2f2] h-screen">
+      <div className="mx-auto max-w-screen-xl px-4 py-14 lg:flex  flex-col ">
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
             Build Form in
@@ -37,47 +37,47 @@ const Hero = () => {
             </a>
           </div>
         </div>
-        <div className="mt-[45px] text-[35px] font-extrabold text-center">
-          How It Works ?
+      </div>
+      <div className=" text-[35px] font-extrabold text-center">
+        How It Works ?
+      </div>
+      <div className=" flex flex-col md:flex-row gap-5  justify-center  items-center  p-10 md:p-14">
+        <div className=" shadow-lg bg-[#ffff] rounded-xl px-4 py-6 h-[180px] md:w-[800px]">
+          <div className="flex items-center gap-3">
+            <MdOutlineFormatAlignLeft className="h-[25px] w-[25px] text-black" />
+            <div className=" text-[20px] font-bold">
+              Write a Prompt for your Form
+            </div>
+          </div>
+          <div className="text-slate-600 mt-2 text-sm ">
+            Craft a clear and concise prompt that outlines the purpose of your
+            form.your prompt will guide the AI in generating relevant fields and
+            questions tailored to your requirements.
+          </div>
         </div>
-        <div className=" flex flex-col md:flex-row gap-5  justify-center mt-[50px] items-center ">
-          <div className=" shadow-lg bg-[#ffff] rounded-xl px-4 py-6 h-[150px] md:w-[500px]">
-            <div className="flex items-center gap-3">
-              <MdOutlineFormatAlignLeft className="h-[25px] w-[25px] text-black" />
-              <div className=" text-[20px] font-bold text-center">
-                Write a Prompt for your Form
-              </div>
-            </div>
-            <div className="text-slate-600 mt-2 text-sm ">
-              Craft a clear and concise prompt that outlines the purpose of your
-              form.your prompt will guide the AI in generating relevant fields
-              and questions tailored to your requirements.
+        <div className=" shadow-lg bg-[#ffff] rounded-xl px-4 py-6 h-[180px] md:w-[800px]">
+          <div className="flex items-center gap-3">
+            <CiEdit className="h-[25px] w-[25px] text-black" />
+            <div className=" text-[20px] font-bold text-center">
+              Edit Your Form
             </div>
           </div>
-          <div className=" shadow-lg bg-[#ffff] rounded-xl px-4 py-6 h-[150px] md:w-[500px]">
-            <div className="flex items-center gap-3">
-              <CiEdit className="h-[25px] w-[25px] text-black" />
-              <div className=" text-[20px] font-bold text-center">
-                Edit Your Form
-              </div>
-            </div>
-            <div className="text-slate-600 mt-2 text-sm ">
-              Take a moment to review and refine it. You can easily modify any
-              field, adjust question types, or add new elements to ensure the
-              form meets your specific needs.
+          <div className="text-slate-600 mt-2 text-sm ">
+            Take a moment to review and refine it. You can easily modify any
+            field, adjust question types, or add new elements to ensure the form
+            meets your specific needs.
+          </div>
+        </div>
+        <div className=" shadow-lg bg-[#ffff] rounded-xl px-4 py-6 h-[180px] md:w-[800px]">
+          <div className="flex items-center gap-3">
+            <IoMdShare className="h-[25px] w-[25px] text-black" />
+            <div className=" text-[20px] font-bold ">
+              Share & Start Accepting Responses
             </div>
           </div>
-          <div className=" shadow-lg bg-[#ffff] rounded-xl px-4 py-6 h-[150px] md:w-[500px]">
-            <div className="flex items-center gap-3">
-              <IoMdShare className="h-[25px] w-[25px] text-black" />
-              <div className=" text-[20px] font-bold ">
-                Share & Start Accepting Responses
-              </div>
-            </div>
-            <div className="text-slate-600 mt-2 text-sm">
-              Share with your audience through link via social media and As the
-              responses coming in we will make the data collection effortless
-            </div>
+          <div className="text-slate-600 mt-2 text-sm">
+            Share with your audience through link via social media and As the
+            responses coming in we will make the data collection effortless
           </div>
         </div>
       </div>
