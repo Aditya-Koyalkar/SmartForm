198358202ac5be0052a05d8ad9c09bae02ae4827
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-10-07 16:23:03 +0530

    payment gateway

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,14 +1,25 @@
 "use client";
+import { GetAllUserForms } from "@/app/actions/GetAllUserForm";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { LibraryBig, LineChart, MessageSquare, Shield } from "lucide-react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Form } from "./FromList";
 
 export const SideNav = () => {
   const path = usePathname();
-  useEffect(() => {}, [path]);
+  const [formList, setFormList] = useState<Form[]>([]);
+  const user = useSession();
+  useEffect(() => {
+    user.data && fetchUserForms();
+  }, [user]);
+  const fetchUserForms = async () => {
+    const forms = await GetAllUserForms(user.data?.user?.email as string);
+    setFormList(forms ? forms : []);
+  };
 
   const menuList = [
     {
@@ -56,9 +67,11 @@ export const SideNav = () => {
       <div className="fixed bottom-10 p-6 w-64">
         <Button className="w-full">+ Create Form</Button>
         <div className="my-7">
-          <Progress value={33} />
+          <Progress
+            value={33 * formList.length < 100 ? 33 * formList.length : 100}
+          />
           <div className="text-sm mt-2 text-gray-600">
-            <b> 2 out of 3</b> File Created
+            <b> {formList.length} out of 3</b> File Created
           </div>
           <div className="text-sm mt-2 text-gray-600">
             Upgrade your plan for unlimited AI forms
---
+++ app/dashboard/upgrade/_data/Plan.ts
@@ -0,0 +1,14 @@
+export const PricingPlan = [
+  {
+    link: "https://buy.stripe.com/test_cN23eLfdA3ddaf6bIJ",
+    price: "3.00",
+    priceId: "price_1Q7E8FIGs8yfTNC8w2zlswco",
+    duration: "Monthly",
+  },
+  {
+    link: "https://buy.stripe.com/test_cN24iP3uS6pp5YQ8ww",
+    price: "30.00",
+    priceId: "price_1Q7DzqIGs8yfTNC8Wbs4PhMM",
+    duration: "Yearly",
+  },
+];
