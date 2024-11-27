a5ab949e94f30543146c3f4e0075ec221241767f
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-11-27 22:54:36 +0530

    fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,11 +2,11 @@ import { SideNav } from "./_components/SideNav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
+    <div className="flex w-full">
       <div className="hidden  md:block md:w-64 ">
         <SideNav />
       </div>
-      <div>{children}</div>
+      <div className="flex-1">{children}</div>
     </div>
   );
 }
